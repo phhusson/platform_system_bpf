1b8962af4c1b26251e83ee4593300d439e9922aa
Joel Fernandes <joelaf@google.com>
2019-04-17 11:30:46 -0400

    Add support for the bpf_probe_read_str bpf function
    This is needed for the iorapd project (i/o readahead).
    
    Change-Id: If6a3de682296bbce0fc808a0eced6bd778106292
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- progs/include/bpf_helpers.h
+++ progs/include/bpf_helpers.h
@@ -17,6 +17,7 @@ static int (*bpf_map_update_elem)(void* map, void* key, void* value,
                                   unsigned long long flags) = (void*) BPF_FUNC_map_update_elem;
 static int (*bpf_map_delete_elem)(void* map, void* key) = (void*) BPF_FUNC_map_delete_elem;
 static int (*bpf_probe_read)(void* dst, int size, void* unsafe_ptr) = (void*) BPF_FUNC_probe_read;
+static int (*bpf_probe_read_str)(void* dst, int size, void* unsafe_ptr) = (void*) BPF_FUNC_probe_read_str;
 static unsigned long long (*bpf_ktime_get_ns)(void) = (void*) BPF_FUNC_ktime_get_ns;
 static int (*bpf_trace_printk)(const char* fmt, int fmt_size, ...) = (void*) BPF_FUNC_trace_printk;
 static unsigned long long (*bpf_get_current_pid_tgid)(void) = (void*) BPF_FUNC_get_current_pid_tgid;
