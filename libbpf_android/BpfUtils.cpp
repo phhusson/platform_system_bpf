fd90bc8ba1ef8005f68d158b73bad1e76fbd5d2d
Maciej Żenczykowski <maze@google.com>
2020-02-10 19:27:47 -0800

    make getBpfSupportLevel() cached
    Test: build, atest
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I6948934c88ce8268ad38e8c0061807a81f5ea4bb

--- libbpf_android/BpfUtils.cpp
+++ libbpf_android/BpfUtils.cpp
@@ -106,7 +106,7 @@ std::string BpfLevelToString(BpfLevel bpfLevel) {
     }
 }
 
-BpfLevel getBpfSupportLevel() {
+static BpfLevel getUncachedBpfSupportLevel() {
     struct utsname buf;
     int kernel_version_major;
     int kernel_version_minor;
@@ -133,6 +133,18 @@ BpfLevel getBpfSupportLevel() {
     if (kernel_version_major == 4 && kernel_version_minor >= 9) return BpfLevel::BASIC;
 
     return BpfLevel::NONE;
+}
+
+BpfLevel getBpfSupportLevel() {
+    static bool initialized = false;
+    static BpfLevel cache;
+
+    if (!initialized) {
+        initialized = true;
+        cache = getUncachedBpfSupportLevel();
+    }
+
+    return cache;
 }
 
 }  // namespace bpf
